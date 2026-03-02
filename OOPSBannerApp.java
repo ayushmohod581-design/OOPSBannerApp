807e78e9a0a8318ed650b58958568c201820efe8
... <ayushmohod581@gmail.com>
2026-03-02 09:04:56 +0530

    Added OOPSBannerApp project

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,17 @@
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("  *****     *****     ******   ****** ");
-        System.out.println(" *     *   *     *    *     *  *      ");
-        System.out.println("*       * *       *   *     *  *      ");
-        System.out.println("*       * *       *   ******    ***** ");
-        System.out.println("*       * *       *   *              *");
-        System.out.println(" *     *   *     *    *              *");
-        System.out.println("  *****     *****     *        ****** ");
 
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "********************",
+            "*                  *",
+            "*     OOPS APP     *",
+            "*                  *",
+            "********************"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
