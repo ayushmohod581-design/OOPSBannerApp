0a866309c80a9fd4902ac0d693706f2d1ed1293c
... <ayushmohod581@gmail.com>
2026-03-02 09:32:39 +0530

    Updated banner using println statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,16 +2,12 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            "********************",
-            "*                  *",
-            "*     OOPS APP     *",
-            "*                  *",
-            "********************"
-        };
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *       *      ");
+        System.out.println("*     * *     * *       *      ");
+        System.out.println(" *****   *****   *        ***** ");
     }
 }
