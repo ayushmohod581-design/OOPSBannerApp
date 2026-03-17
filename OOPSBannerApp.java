f3f8d07706b99388ab1f77000859a40fb89e451e
... <ayushmohod581@gmail.com>
2026-03-17 09:30:23 +0530

    Uploded UC6 using static method (modular approach)

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,62 @@
-public class HelloArgsApp {
+public class OOPSBannerApp {
+
+    public static String[] getO() {
+        return new String[]{
+            " OOO ",
+            "O   O",
+            "O   O",
+            "O   O",
+            " OOO "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[]{
+            "PPPP ",
+            "P   P",
+            "PPPP ",
+            "P    ",
+            "P    "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+            " SSS ",
+            "S    ",
+            " SSS ",
+            "    S",
+            " SSS "
+        };
+    }
+
+    public static String[] getA() {
+        return new String[]{
+            " AAA ",
+            "A   A",
+            "AAAAA",
+            "A   A",
+            "A   A"
+        };
+    }
+
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            for (String name : args) {
-                System.out.println("Hello, " + name);
-            }
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+        String[] A = getA();
+
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(
+                O[i] + "  " + 
+                O[i] + "  " + 
+                P[i] + "  " + 
+                S[i] + "  " +
+                A[i] + "  " +
+                P[i] + "  " +
+                P[i]
+            );
         }
     }
 }
