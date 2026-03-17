272f61ff2d5f3b9025d5f1bf34b325b1e5ebf7e4
... <ayushmohod581@gmail.com>
2026-03-17 09:18:20 +0530

    Added UC6 HelloPatternApp

---
+++ HelloPatternApp.java
@@ -0,0 +1,11 @@
+public class HelloPatternApp {
+    public static void main(String[] args) {
+
+        System.out.println("H     H  EEEEE  L      L       OOOOO");
+        System.out.println("H     H  E      L      L       O   O");
+        System.out.println("HHHHHHH  EEEE   L      L       O   O");
+        System.out.println("H     H  E      L      L       O   O");
+        System.out.println("H     H  EEEEE  LLLLL  LLLLL   OOOOO");
+
+    }
+}
